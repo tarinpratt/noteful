bdacb7472cdda6f612e0f8b6534df182b80bd930
Tarin Pratt <tarinpratt@gmail.com>
2019-08-29 21:29:37 -0700

    delpoyed

--- src/note/note.test.js
+++ src/note/note.test.js
@@ -8,21 +8,13 @@ import Note from './Note'
 
 
 describe(`Note component`, () => {
-  const props = {
-    id: 'x',
-    name: 'name',
-    modified: new Date(2019, 7, 26),
-  }
+ 
 
   it('renders a Note', () => {
     const wrapper = shallow(<Note />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('renders a Note w/ props', () => {
-    const wrapper = shallow(<Note {...props} />)
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
