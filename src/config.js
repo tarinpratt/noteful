364b73b6e5279d245fb38da77bfb9079f9559971
Tarin Pratt <tarinpratt@gmail.com>
2019-08-29 18:20:59 -0700

    final commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://glacial-bastion-54172.herokuapp.com/'
   }
   
--- src/noteContent/noteContent.test.js
+++ src/noteContent/noteContent.test.js
@@ -6,24 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import NoteContent from './noteContent'
 
 describe(`NoteContent component`, () => {
-const props = {
-  content: "Voluptates sunt eligendi",
-  folderId: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-  id: "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
-  modified: "2019-01-03T00:00:00.000Z",
-  name: "Dogs"
-}
-
-it('renders Note Content', () => {
-  const wrapper = shallow(<NoteContent />)
-  expect(toJson(wrapper)).toMatchSnapshot()
-})
-
-it('renders a Note with note prop', () => {
-  const note = shallow(<NoteContent {...props} />)
-    .find('Note')
-  expect(toJson(note)).toMatchSnapshot()
-})
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
